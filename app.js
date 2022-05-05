65c64b990b7fdb0a70931cfd44dbcb6cec8fe5b5
quintasluizdenis <quintasluizdenis@gmail.com>
2022-05-05 14:50:38 +0000

    adicionado a tabela e geração de panilha excel

--- app.js
+++ app.js
@@ -3,8 +3,16 @@
 const btnEnvio = document.querySelector("#btn-enviar");
 //selecionado o campo de entrada
 const inputCodigoEscolar = document.querySelector("#btn-codigoescolar");
+//selecionando o botao panilha
+const btnPanilha = document.querySelector("#panilha-container button");
 
-//adicionando o evento ao botao
+//adicionando evento ao botao que faz a criação da panilha
+btnPanilha.addEventListener("click", (e)=>{
+    //função que converte a tabela em panilha excel
+    TableToExcel.convert(document.querySelector("div table"))
+})
+
+//adicionando o evento ao botao enviar
 btnEnvio.addEventListener("click", async (e) =>{
     e.preventDefault();
     //pegando o valor do input
@@ -16,20 +24,21 @@ btnEnvio.addEventListener("click", async (e) =>{
 
         //enviando a requisição para o servidor
         let resultado = await fetch(uRL, {mode: "cors"});
-        //filtrando o resultado em um json mais limpo
+        //covertento a resposta do servidor em objeto json()
         let json = await resultado.json();
         
-        //verificando se houve o response do servidor
-        //alert("O Cep informado não existe!");
+       //mostrando a resposta do servidor na console
         console.log(json);
-        
+
+         //verificando se houve o response do servidor
         if(json.result != null || json.result != undefined){
+            //fazendo o consumo do json
             showInfor({
                 nome: json.result.nome,
                 apelido: json.result.apelido,
                 turma: json.result.turma,
                 curso: json.result.curso,
-                polouniversitario: json.result.polouniversitario,  //polounivesitario
+                polouniversitario: json.result.polouniversitario,  
                 numerocadeiraematraso: json.result.numerocadeiraematraso,
                 maiornotapauta: json.result.maiornotapauta,
                 menornotapauta: json.result.menornotapauta
@@ -43,17 +52,16 @@ btnEnvio.addEventListener("click", async (e) =>{
 })
 
 /*Implementado a funcão que mostra o resultado */
-
 function showInfor(json){
-    document.querySelector("#itens-container #nome").innerHTML = `${json.nome}`;
-    document.querySelector("#itens-container #apelido").innerHTML = `${json.apelido}`;
-    document.querySelector("#itens-container #turma").innerHTML = `${json.turma}`;
-    document.querySelector("#itens-container #curso").innerHTML = `${json.curso}`;
+    document.querySelector("#table-container tr #nome").innerHTML = `${json.nome}`;
+    document.querySelector("#table-container tr #apelido").innerHTML = `${json.apelido}`;
+    document.querySelector("#table-container tr #turma").innerHTML = `${json.turma}`;
+    document.querySelector("#table-container tr #curso").innerHTML = `${json.curso}`;
 
-    document.querySelector("#itens-container #polouniversitario").innerHTML = `${json.polouniversitario}`;
-    document.querySelector("#itens-container #numerocadeiraematraso").innerHTML = `${json.numerocadeiraematraso}`;
-    document.querySelector("#itens-container #maiornotapauta").innerHTML = `${json.maiornotapauta}`;
-    document.querySelector("#itens-container #menornotapauta").innerHTML = `${json.menornotapauta}`;
+    document.querySelector("#table-container tr #polouniversitario").innerHTML = `${json.polouniversitario}`;
+    document.querySelector("#table-container tr #numerocadeiraematraso").innerHTML = `${json.numerocadeiraematraso}`;
+    document.querySelector("#table-container tr #maiornotapauta").innerHTML = `${json.maiornotapauta}`;
+    document.querySelector("#table-container tr #menornotapauta").innerHTML = `${json.menornotapauta}`;
 
     document.querySelector(".hiden").classList.remove("hiden");
 
